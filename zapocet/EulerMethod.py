3755f4e1ab7479b595d5b2a184c255cb4a9d3cc0
David Moravec <david.moravec@cfdsupport.com>
2020-05-17 18:09:18 +0200

    oprava, ted uz neco pocita mozna bude i zapocet

--- zapocet/EulerMethod.py
+++ zapocet/EulerMethod.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 import misc
 import matplotlib.pyplot as plt
 
@@ -31,6 +32,7 @@ class EulerMethod:
       for i in range(1, len(U_new) - 1):
         U_new[i] = U_old[i] + r * (U_old[i-1] - 2 * U_old[i] + U_old[i+1])
 
+      self.Plot(U_new)
       U_old = misc.Update(U_old, U_new)
 
     self.expl = U_new
@@ -47,11 +49,19 @@ class EulerMethod:
       U_new = np.linalg.solve(self.matrix, U_old) 
       #if t % 100 == 1:
         #print(t, U_new)
-
+      self.Plot(U_new)
       U_old = misc.Update(U_old, U_new)
 		
     self.impl = U_new
     print(self.impl)
+
+  def Plot(self, data):
+    x = self.x
+    plot = plt.figure()
+    plt.plot(x, data)
+    plt.show(block=False)
+    time.sleep(0.2)
+    plt.close(plot)
 
   def PlotResults(self):
     x = self.x
@@ -60,4 +70,4 @@ class EulerMethod:
     plt.plot(x, self.impl, label='implicit')
     plt.plot(x, self.expl, label='expl')
     plt.legend(loc='upper left')
-    plt.show()
+    plt.show(block=False)
